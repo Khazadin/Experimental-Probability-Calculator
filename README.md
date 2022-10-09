b8fc6fc191ccfdb5970d54ba6fb6e633a05ff187
Derviş Mehmet USTA <dervismehmetusta@Dervis-MacBook-Pro.local>
2022-10-09 19:03:52 +0300

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+# Dice-Exp.-Prob.-Calculator
--- script.js
+++ script.js
@@ -1,84 +1,62 @@
 "use strict";
-
-///////////////////////////
 function refreshPage() {
   window.location.reload();
 }
 ///////////////////////////
-var dice = {
-  sides: 6,
-  roll: function () {
-    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-    return randomNumber;
-  },
+
+let meto = function () {
+  let dice = Math.trunc(Math.random() * 6) + 1;
+  return dice;
 };
-function printNumber(number) {
-  var placeholder = document.getElementById("placeholder");
-  placeholder.innerHTML = number;
-}
-
-var button = document.getElementById("button1");
-var result;
-let bir = [];
-let bir1 = [];
-let bir2 = [];
-let bir3 = [];
-let bir4 = [];
-let bir5 = [];
-
-let birort = 0;
-let birsayi = 0;
-let iki = 0;
-let uc = 0;
-let dort = 0;
-let bes = 0;
-let alti = 0;
-
-button.onclick = function () {
-  result = dice.roll();
-  printNumber(result);
-  if (result == 1) {
-    bir.push("1");
-    birsayi = bir.length;
-    document.getElementById("oners").textContent = birsayi;
-    birort = birsayi / (birsayi + iki + uc + dort + bes + alti);
-    document.getElementById("birort").textContent = birort;
-  }
-  if (result == 2) {
-    bir1.push("1");
-    iki = bir1.length;
-    document.getElementById("twoers").textContent = iki;
-    birort = iki / (birsayi + iki + uc + dort + bes + alti);
-    document.getElementById("ikiort").textContent = birort;
-  }
-  if (result == 3) {
-    bir2.push("1");
-    uc = bir2.length;
-    document.getElementById("thrers").textContent = uc;
-    birort = uc / (birsayi + iki + uc + dort + bes + alti);
-    document.getElementById("ucort").textContent = birort;
-  }
-  if (result == 4) {
-    bir3.push("1");
-    dort = bir3.length;
-    document.getElementById("fouers").textContent = dort;
-    birort = dort / (birsayi + iki + uc + dort + bes + alti);
-    document.getElementById("dortort").textContent = birort;
-  }
-  if (result == 5) {
-    bir4.push("1");
-    bes = bir4.length;
-    document.getElementById("fivers").textContent = bes;
-    birort = bes / (birsayi + iki + uc + dort + bes + alti);
-    document.getElementById("besort").textContent = birort;
-  }
-  if (result == 6) {
-    bir5.push("1");
-    alti = bir5.length;
-    document.getElementById("sixers").textContent = alti;
-    birort = alti / (birsayi + iki + uc + dort + bes + alti);
-    document.getElementById("altiort").textContent = birort;
-  }
-  let tot = birsayi + iki + uc + dort + bes + alti;
-  document.getElementById("totto").textContent = tot;
+let IncrementDiv = (div_id) => {
+  let my_div = document.getElementById(div_id);
+  my_div.innerText = parseInt(my_div.innerText) + 1;
 };
+let calcrate = function (result, input) {
+  let my_span = document.getElementById(result);
+  console.log("=========================");
+  console.log(document.getElementById(input).innerText);
+  console.log(document.getElementById("totto").innerText);
+  console.log("=========================");
+
+  my_span.innerText =
+    parseFloat(document.getElementById(input).innerText) /
+    parseFloat(document.getElementById("totto").innerText);
+};
+let calcrates = function () {
+  calcrate("birort", "oners");
+  calcrate("ikiort", "twoers");
+  calcrate("ucort", "thrers");
+  calcrate("dortort", "fouers");
+  calcrate("besort", "fivers");
+  calcrate("altiort", "sixers");
+};
+let buttonselector = document.getElementById("button1");
+let diceprinter = document.getElementById("placeholder");
+buttonselector.addEventListener("click", function () {
+  let result = meto();
+  IncrementDiv("totto");
+  diceprinter.innerHTML = result;
+
+  switch (result) {
+    case 1:
+      IncrementDiv("oners");
+      break;
+    case 2:
+      IncrementDiv("twoers");
+      break;
+    case 3:
+      IncrementDiv("thrers");
+      break;
+    case 4:
+      IncrementDiv("fouers");
+      break;
+    case 5:
+      IncrementDiv("fivers");
+      break;
+    case 6:
+      IncrementDiv("sixers");
+      break;
+  }
+  calcrates();
+});
